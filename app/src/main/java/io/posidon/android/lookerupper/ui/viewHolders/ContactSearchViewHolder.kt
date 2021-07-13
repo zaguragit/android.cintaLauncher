687c0e2b5232b4aba65a111104ef9606f7a47d02
leoxshn <leo@posidon.io>
2021-07-13 16:12:50 +0200

    Improved search results' ui

--- app/src/main/java/io/posidon/android/lookerupper/ui/viewHolders/ContactSearchViewHolder.kt
+++ app/src/main/java/io/posidon/android/lookerupper/ui/viewHolders/ContactSearchViewHolder.kt
@@ -3,7 +3,11 @@ package io.posidon.android.lookerupper.ui.viewHolders
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import io.posidon.android.cintalauncher.R
+import io.posidon.android.cintalauncher.ui.color.ColorTheme
 import io.posidon.android.lookerupper.data.results.ContactResult
 import io.posidon.android.lookerupper.data.results.SearchResult
 
@@ -14,8 +18,15 @@ class ContactSearchViewHolder(itemView: View) : SearchViewHolder(itemView) {
 
     override fun onBind(result: SearchResult) {
         result as ContactResult
-        icon.setImageURI(result.iconUri)
+        Glide.with(itemView)
+            .load(result.iconUri)
+            .placeholder(ContextCompat.getDrawable(itemView.context, R.drawable.placeholder_contact)!!.apply {
+                setTint(ColorTheme.hintColorForBG(itemView.context, ColorTheme.feedBG))
+            })
+            .apply(RequestOptions.circleCropTransform())
+            .into(icon)
         text.text = result.title
+        text.setTextColor(ColorTheme.titleColorForBG(itemView.context, ColorTheme.feedBG))
         itemView.setOnClickListener(result::open)
     }
 }
